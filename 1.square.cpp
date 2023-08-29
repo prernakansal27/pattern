f890ae04906b8544f58b0afe13084401dbd8234c
prernakansal27 <96967316+prernakansal27@users.noreply.github.com>
2023-08-29 15:20:00 +0530

    1.square.cpp

---
+++ 1.square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ //square//
+ for(int row=0;row<4;row++){
+  for(int j=0;j<4;j++){
+    cout<<"*";
+  }
+  cout<<endl;
+ }
+  return 0;
+}
