64ec3fa1f01d7cb3c37aa7d6fad0009c1b44232f
prernakansal27 <96967316+prernakansal27@users.noreply.github.com>
2023-08-29 15:21:30 +0530

    2.rectangle.cpp

---
+++ 2.rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ //rectangle//
+ for(int row=0;row<4;row++){
+  for(int j=0;j<10;j++){
+    cout<<"*";
+  }
+  cout<<endl;
+ }
+  return 0;
+}
